f0e16064a29f18cc2eba8a30c1084cfc9ceb16ac
Maria Guerrero <guerrermj@gmail.com>
2020-03-08 03:18:56 -0400

    cleaned up js file and added more jquery

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 //////////// MOBILE NAV FUNCTIONALITY ////////////
+function displayProjects() {
+  $(document).ready(function() {
+    $(".proj-demo").append(`<img src='./imgs/app-sc.png'>`);
+    $(".proj-desc").append(` 
+  <h3 class="proj-header">What's it about?</h3>
+  <p>A quiz app that tests your knowledge on all things sleep—from dreams
+  and nightmares, sleep deprivation, and more. Find out if you know
+  what really goes on while you sleep.</p>`);
+    $(".proj-tech").append(`<h3 class="proj-header">Tech Stack</h3>
+  <ul>
+  <li><i class="fab fa-js"></i></li>
+  <li><i class="fab fa-html5"></i></li>
+  <li><i class="fab fa-css3-alt"></i></li>
+  <li><i class="fab fa-github"></i></li> 
+</ul>
+    <p> jQuery, JavaScript, HTML5, CSS3, Git, Github</p>`);
+  });
+}
 
-$("#toggle").click(function() {
-  $(this).toggleClass("toggle-active");
-  $("#overlay")
-    .toggleClass("nav-overlay-active")
-    .show();
-});
+function mobileMenuToggled() {
+  $("#toggle").click(function() {
+    $(this).toggleClass("toggle-active");
+    $("#overlay")
+      .toggleClass("nav-overlay-active")
+      .show();
+  });
 
-// Close after clicking on a menu option
-$(".nav-overlay li").on("click", function() {
-  $("#overlay").hide();
-});
+  // Close after clicking on a menu option
+  $(".nav-overlay li").on("click", function() {
+    let width = $(window).width();
+    if (width < 913) {
+      $("#overlay").hide();
+    }
+  });
+  $("#overlay").on("scroll touchmove mousewheel", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
+  });
+}
+
+$(displayProjects);
+$(mobileMenuToggled);
