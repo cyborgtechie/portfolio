852a0d86bfaf1b586f4132ce672af54e6e99aed9
Maria Guerrero <guerrermj@gmail.com>
2021-02-12 01:33:12 -0500

    project section updated

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //////////// MOBILE NAV FUNCTIONALITY ////////////
 function displayProjects() {
-  $(document).ready(function() {
-    $(".proj-demo").append(`<img src='./imgs/app-sc.png' alt="screenshot of sleep quiz project">`);
-    $(".proj-desc").append(` 
+  $(document).ready(function () {
+    $(".d1").append(
+      `<img src='./imgs/app-sc.png' alt="screenshot of sleep quiz project">`
+    );
+    $(".pd1").append(` 
   <h3 class="proj-header">What's it about?</h3>
   <p>A quiz app that tests your knowledge on all things sleep—from dreams
   and nightmares, sleep deprivation, and more. Find out if you know
   what really goes on while you sleep.</p>`);
-    $(".proj-tech").append(`<h3 class="proj-header">Tech Stack</h3>
+    $(".t1").append(`<h3 class="proj-header">Tech Stack</h3>
   <ul>
   <li><i class="fab fa-js"></i></li>
   <li><i class="fab fa-html5"></i></li>
@@ -16,30 +18,42 @@ function displayProjects() {
 </ul>
     <p> jQuery, JavaScript, HTML5, CSS3, Git, Github</p>`);
   });
+  $(document).ready(function () {
+    $(".d2").append(
+      `<img src='./imgs/proj2-sc.png' alt="screenshot of find us project">`
+    );
+    $(".pd2").append(` 
+  <h3 class="proj-header">What's it about?</h3>
+  <p>An app that provides you information on missing persons in your area or state. This app includes their last known location, description of the person, and resources you can utilize to help find them.</p>`);
+    $(".t2").append(`<h3 class="proj-header">Tech Stack</h3>
+  <ul>
+  <li><i class="fab fa-js"></i></li>
+  <li><i class="fab fa-html5"></i></li>
+  <li><i class="fab fa-css3-alt"></i></li>
+  <li><i class="fab fa-github"></i></li> 
+</ul>
+    <p>Javascript, JQuery, HTML5, CSS, Chart.js, Fetch API, Git</p>`);
+  });
 }
 
 function mobileMenuToggled() {
-  $("#toggle").on("click", function() {
+  $("#toggle").on("click", function () {
     $(this).toggleClass("toggle-active");
-    $("#overlay")
-      .toggleClass("nav-overlay-active")
-      .show();
+    $("#overlay").toggleClass("nav-overlay-active").show();
   });
 
-  $("#overlay").on("scroll touchmove mousewheel", function(e) {
+  $("#overlay").on("scroll touchmove mousewheel", function (e) {
     e.preventDefault();
     e.stopPropagation();
     return false;
   });
 
   // Close after clicking on a menu option
-  $("nav li ").on("click", function() {
+  $("nav li ").on("click", function () {
     $(this).show();
     let width = $(window).width();
     if (width < 913) {
-      $("#overlay")
-        .toggleClass("nav-overlay-active")
-        .hide();
+      $("#overlay").toggleClass("nav-overlay-active").hide();
       $(".hamburger").toggleClass("toggle-active");
     }
   });
